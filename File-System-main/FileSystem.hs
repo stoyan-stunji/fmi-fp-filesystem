a73886a04b28af602ef052ab9d0265db31327ec0
Stoyan Ivanov <115835362+stoyan-stunji@users.noreply.github.com>
2024-12-29 19:35:40 +0200

    Delete File-System-main directory

--- File-System-main/FileSystem.hs
+++
@@ -1,6 +0,0 @@
-module FileSystem where
-     
-data FileSystem =
-     File String String     | -- File <name> <Value>, only support .txt
-     Root String [FileSystem] -- Root <name> [<content>]
-     deriving(Show)
